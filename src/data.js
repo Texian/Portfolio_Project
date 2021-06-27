9a4567c877ef89b648f0cd1d2d9b22fb6f7ecf09
Christian Walters <xian04@yahoo.com>
2021-06-27 16:34:33 -0700

    Skeleton set up

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum",
+     image: "./project1.gif",
+     link: "https://reactbootcamp.com",
+  }, 
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum",
+     image: "./project2.gif",
+     link: "https://reedbarger.com",
+  }, 
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum",
+     image: "./project3.gif",
+     link: "https://jsbootcamp.com",
+  }, 
+  {
+    title: "Epic todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum",
+     image: "./project4.gif",
+     link: "https://pythonbootcamp.com",
+  },
+];
